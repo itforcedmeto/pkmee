aefc79e5ff71f05e81fd2c2aebdfc723f46ca0a3
Josh Dennis <joshdennis93@gmail.com>
2024-11-16 23:38:30 +1100

    up to tm52

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1218,12 +1218,12 @@
 #define FLAG_ITEM_SAFARI_ZONE_NORTH_EAST_NUGGET                     0x491
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 
-#define FLAG_UNUSED_0x493                                           0x493 // Unused Flag
-#define FLAG_UNUSED_0x494                                           0x494 // Unused Flag
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
-#define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
-#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
-#define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
+#define FLAG_ITEM_ROUTE_105_ICYWIND                                 0x493 // Unused Flag
+#define FLAG_RECEIVED_TM_PROTECT                                    0x494 // Unused Flag
+#define FLAG_ITEM_SKY_PILLAR_DRAGONPULSE                            0x495 // Unused Flag
+#define FLAG_ITEM_MAGMA_HIDEOUT_FLAMETHROWER                        0x496 // Unused Flag
+#define FLAG_ITEM_SAFARI_ZONE_DAZZLINGGLEAM                         0x497 // Unused Flag
+#define FLAG_RECEIVED_TM_TRICK_ROOM                                           0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
 #define FLAG_UNUSED_0x49B                                           0x49B // Unused Flag
